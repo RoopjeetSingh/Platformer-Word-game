25f7d9bbefb15126e1918f9291a2d9f9a692d8be
tanman1049 <121959929+tanman1049@users.noreply.github.com>
2023-01-10 10:31:51 -0500

    Update wordconnect.py

--- wordconnect.py
+++ wordconnect.py
@@ -4,58 +4,65 @@ from pygame.locals import *
 from pygame import mixer
 import math
 
-
 py.init()
 mixer.init()
 mixer.music.load("./hellop/success-1-6297.mp3")
 screen = py.display.set_mode((1000, 500))
-screen.fill((255,255,255))
+screen.fill((255, 255, 255))
 letters = ["a", "b", "c", "d", "e"]
 
-#find better images here
-list_images = {"a": py.image.load("./hellop/im.png"), "b": py.image.load("./hellop/b.png"),"c":py.image.load("./hellop/images (1).png"),"d": py.image.load("./hellop/d.png"), "e":py.image.load("hellop/6422859.png")}
+# find better images here
+list_images = {"a": py.image.load("./hellop/im.png"), "b": py.image.load("./hellop/b.png"),
+               "c": py.image.load("./hellop/images (1).png"), "d": py.image.load("./hellop/d.png"),
+               "e": py.image.load("hellop/6422859.png")}
 coord = []
 entered = []
-correct_words = ["dabc"]
+correct_words = ["dabc", "ebcd"]
 word = ""
 
 
-def background():
+def background(x,y,z):
     bg_image = py.image.load("hellop/winter.jpg")
     bg_image = py.transform.scale(bg_image, (1000, 500))
     table = py.Surface((400, 100))
     table.set_alpha(128)
-    table.fill((255,255,255))
-    screen.blit(bg_image, (0,0))
+    table.fill((x,y,z))
+    screen.blit(bg_image, (0, 0))
     screen.blit(table, (300, 50))
+
 
 def place(n):
     global coord
     a = 0
-    adding = (2*3.14) / n
+    adding = (2 * 3.14) / n
     for i in range(0, n):
         im = list_images[letters[i]]
         im = py.transform.scale(im, (35, 35))
-        screen.blit(im, (480 + 130*math.cos(a), 320+ 130*math.sin(a)))
-        coord.append((480 + 130*math.cos(a), 320+ 130*math.sin(a)))
+        screen.blit(im, (480 + 130 * math.cos(a), 320 + 130 * math.sin(a)))
+        coord.append((480 + 130 * math.cos(a), 320 + 130 * math.sin(a)))
         a += adding
+
 
 def near(x, y):
     z = []
     for i in range(len(x)):
-        z.append((math.sqrt(pow(x[i][0] - y[0], 2)) +math.sqrt(pow(x[i][1] - y[1],2))))
+        z.append((math.sqrt(pow(x[i][0] - y[0], 2)) + math.sqrt(pow(x[i][1] - y[1], 2))))
 
     return x[z.index(min(z))]
 
+
 def show():
     global word
-    global count
+
     adding = 30
     subtraction = 15
     for i in range(len(word)):
         im = list_images[word[i]]
-        im = py.transform.scale(im, (25,25))
-        screen.blit(im, (500 + (adding * i )-( subtraction* len(word)),90 ))
+        im = py.transform.scale(im, (25, 25))
+        screen.blit(im, (500 + (adding * i) - (subtraction * len(word)), 90))
+
+
+
 
 def correct():
     py.mixer.music.play()
@@ -64,7 +71,7 @@ def correct():
 
 
 run = True
-background()
+background(255,255,255)
 place(len(letters))
 start = ()
 
@@ -82,27 +89,26 @@ while run:
             else:
                 word = ""
                 entered = []
-    
-
 
     if start != ():
-        background()
+        background(255,255,255)
         place(len(letters))
         py.draw.line(screen, (34, 153, 153), (start[0] + 20, start[1] + 20), (mouse[0] + 20, mouse[1] + 20), width=5)
         show()
+        if len(word) == len(letters):
+            start= ()
+            entered = []
+            background(201, 47, 4)
+            place(len(letters))
+            show()
+            word = ""
 
-    if word in correct_words:
-
-        place(len(letters))
-        start = ()
-        word = ""
-        entered = []
-    elif len(word) == len(letters) and word not in correct_words:
-
-        start = ()
-        word = ""
-        entered = []
-        background()
-        place(len(letters))
-
+        elif word in correct_words:
+            start= ()
+            entered = []
+            background(235, 235, 35)
+            place(len(letters))
+            show()
+            word = ""
     py.display.update()
+
