4367cbae3f31bd05eca3c2e8b19ff8d2eb53721a
tanman1049 <121959929+tanman1049@users.noreply.github.com>
2023-01-12 12:46:29 -0500

    Update wordconnect.py

--- wordconnect.py
+++ wordconnect.py
@@ -1,16 +1,14 @@
-import time
 import pygame as py
 from pygame.locals import *
-from pygame import mixer
 import math
 
 py.init()
 screen = py.display.set_mode((1000, 500))
 screen.fill((255, 255, 255))
-letters = ["l", "o", "g"]
+letters = ["l", "o", "g", "q", "w", "y"]
 input_rect = py.Rect(200, 200, 140, 32)
-submit_rect = py.Rect(100, 100, 0,0)
-i = 1
+score_count = 0
+i = -1
 list_images = {'a': "hellop/Platformer-word-game-master/images/Letters/1.png", 'b': "hellop/Platformer-word-game-master/images/Letters/9.png",
                 'c': "hellop/Platformer-word-game-master/images/Letters/19.png",
               'd': "hellop/Platformer-word-game-master/images/Letters/15.png", 'e': "hellop/Platformer-word-game-master/images/Letters/26.png",
@@ -30,35 +28,37 @@ list_images = {'a': "hellop/Platformer-word-game-master/images/Letters/1.png", '
               'y': "hellop/Platformer-word-game-master/images/Letters/4.png", 'z': "hellop/Platformer-word-game-master/images/Letters/14.png"}
 coord = []
 entered = []
-correct_words = ["long"]
+correct_words = ["long", "bol"]
 word = ""
 mystery_letter = ""
 mystery_number = 3
-on = False
+rect_pressed = False
+on = True
 pressed = False
 submit_pressed = False
 
 def background(x,y,z, c):
     bg_image = py.image.load("hellop/flat-design-copy-space-winter-background_52683-48883.jpeg")
     bg_image = py.transform.scale(bg_image, (1000, 500))
-    table = py.Surface((400,c))
+    table = py.Surface((420,c))
     table.set_alpha(128)
     table.fill((x,y,z))
     screen.blit(bg_image, (0, 0))
     screen.blit(table, (300, 25))
 
-
 def place(n):
     global on
     global coord
+
     if on == True:
         a = 0
         adding = (2 * 3.14) / n
         for i in range(0, n):
             im = py.image.load(list_images[letters[i]])
             im = py.transform.scale(im, (35, 35))
-            screen.blit(im, (480 + 130 * math.cos(a), 300+ 130 * math.sin(a)))
-            coord.append((480 + 130 * math.cos(a), 300 + 130 * math.sin(a)))
+            screen.blit(im,((480 + 130 * math.cos(a), 300 + 130 * math.sin(a))) )
+            if len(coord) < len(letters):
+                coord.append((480 + 130 * math.cos(a), 300 + 130 * math.sin(a)))
             a += adding
 
 def lines():
@@ -84,76 +84,95 @@ def show():
         screen.blit(im, (500 + (adding * i) - (subtraction * len(word)), 90))
 
 
-def mystery(alphabet):
-    global mystery_letter
-    global input_rect
-    global on
-    global letters
-    adding = 30
-    subtraction = 15
-    for i in range(len(letters)):
-        im = py.image.load(list_images[letters[i]])
-        im = py.transform.scale(im, (25,25))
-        screen.blit(im, (500 + (adding * i) - subtraction* len(word), 90))
-    if on == False:
+def mystery(input):
+    if pressed == True:
         py.draw.rect(screen, (30, 212, 212), input_rect)
-        font = py.font.Font(None,32)
-        mystery_letter = alphabet
-        if pressed == True:
-            py.draw.rect(screen, (95, 204, 0), input_rect)
-            text_pressed = font.render(mystery_letter, True, (255, 255, 255))
-            screen.blit(text_pressed, input_rect)
+        font = py.font.Font(None, 32)
 
+        if rect_pressed == True:
+            py.draw.rect(screen, (95, 204, 0), input_rect)
+            text_pressed = font.render(input, True, (255, 255, 255))
+            if input != "":
+                screen.blit(text_pressed, input_rect)
+
+
+def submit():
+    image = py.image.load("hellop/question.png")
+    image = py.transform.scale(image, (50, 50))
+    screen.blit(image, (0,0))
 
 
 run = True
 background(255,255,255, 100)
-mystery("")
+place(len(letters))
+submit()
 
 start = ()
 clock = py.time.Clock()
 while run:
 
     mouse = py.mouse.get_pos()
+
     for ev in py.event.get():
         if ev.type == QUIT:
             py.quit()
+        if ev.type == KEYDOWN:
+            if mystery_number != 0 and ev.unicode not in letters and rect_pressed == True:
+                if ev.unicode != '\r':
+                    letters.append(ev.unicode)
+                    mystery(ev.unicode)
+                    coord = []
+
+
         if ev.type == MOUSEBUTTONDOWN:
-            if input_rect.collidepoint(mouse):
-                pressed = True
-                mystery("")
+            if 0 < mouse[0] < 50 and 0 < mouse[1] < 50:
+                i += 1
+                if i % 2 == 0:
+                    pressed = True
+                    print(pressed)
+                else:
+                    pressed = False
+                    print(pressed)
+            if input_rect.collidepoint(mouse) and pressed== True:
+                rect_pressed = True
 
+            mystery("")
 
-            if on == True:
+            if on == True and 300 < mouse[0] < 700 and 25 < mouse[1] < 475:
 
                 start = near(coord, mouse)
                 if start not in entered:
+                    print(letters)
+                    print(coord)
+
                     word += letters[coord.index(start)]
                     entered.append(start)
                 else:
                     word = ""
                     entered = []
 
-        if ev.type == KEYDOWN:
-            if mystery_number != 0 and ev.unicode not in letters:
-                if ev.key == K_RETURN:
-                    mystery_number = 0
-                if pressed == True and ev.unicode != '\r':
-                    letters.append(ev.unicode)
-                    mystery_letter = ""
-                    mystery(ev.unicode)
-                mystery_number -= 1
-        if i != 0:
-            if mystery_number == 0:
-                pressed = False
-                on = True
+            if pressed == False:
+
                 background(255, 255, 255, 450)
                 place(len(letters))
-                i -= 1
+                submit()
+                if ev.type == KEYDOWN:
+                    if mystery_number != 0 and ev.unicode not in letters and rect_pressed == True:
+
+                        if ev.unicode != '\r':
+                            letters.append(ev.unicode)
+                            mystery(ev.unicode)
+
+
+
+
+
+
+
 
     if start != ():
         background(255,255,255, 450)
-
+        submit()
         place(len(letters))
         py.draw.line(screen, (34, 153, 153), (start[0] + 20, start[1] + 20), (mouse[0] + 20, mouse[1] + 20), width=5)
         lines()
@@ -162,19 +181,25 @@ while run:
             start= ()
             entered = []
             background(201, 47, 4, 450)
+            submit()
+
             place(len(letters))
             show()
             print(word)
             word = ""
 
         elif word in correct_words:
+            score_count += 1
+
             start= ()
             entered = []
             background(235, 235, 35, 450)
+            submit()
             place(len(letters))
             show()
             print(word)
             word = ""
-
+    #finish()
+    #score()
     clock.tick(50)
     py.display.update()
