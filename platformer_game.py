25bf0f321cdb07b464746293f0289f4dfdc5ad06
RoopjeetSingh <roopjeetsingh2004@gmail.com>
2023-02-01 19:44:13 -0500

    changes

--- platformer_game.py
+++ platformer_game.py
@@ -7,6 +7,7 @@ import json
 from helpful_functions import calculate_current_level
 import time
 from wordconnect import game_Loop_Wordle
+from datetime import time as time_str
 
 pygame.init()
 
@@ -23,7 +24,8 @@ def platformer_game(screen, current_level=None):
                     exit()
             clock.tick()
             pygame.display.flip()
-        game_Loop_Wordle(screen, [letter_obj.letter for letter_obj in player.letter_lis], len(player.mystery_letter_lis))
+        game_Loop_Wordle(screen, [letter_obj.letter for letter_obj in player.letter_lis],
+                         len(player.mystery_letter_lis))
 
     pressed = False
     with open('variables.json', 'r') as f:
@@ -34,7 +36,10 @@ def platformer_game(screen, current_level=None):
     show_time_actual = time.time()
     clock = pygame.time.Clock()
     player = Player(ss.tile_size, ss.tile_size * 2, var["users"][var["current_user"][0]][2])
+    font = pygame.font.SysFont("images/Menu_page/SnowtopCaps.ttf", 75, bold=True)
     while True:
+        show_time -= round(time.time() - show_time_actual)
+        show_time_actual = time.time()
         current_level.draw(screen)
         current_level.obstruct_group.draw(screen)
         current_level.platform_group.draw(screen)
@@ -56,10 +61,16 @@ def platformer_game(screen, current_level=None):
         pressed, killed = player.update_player(screen, current_level, pressed)
         if killed:
             empty_screen()
+
+        print(show_time)
+        time_as_str = time_str(minute=show_time // 60, second=show_time % 60)
+        time_surface = font.render(str(time_as_str), True, (20, 20, 20))
+        time_surface.set_alpha(200)
+        screen.blit(time_surface, (ss.SCREEN_WIDTH - time_surface.get_width() - ss.tile_size, ss.tile_size))
         pygame.display.update()
         clock.tick(90)
 
 
 if __name__ == "__main__":
     root = pygame.display.set_mode((ss.SCREEN_WIDTH, ss.SCREEN_HEIGHT))
-    platformer_game(root, level1)
+    platformer_game(root, level3)
