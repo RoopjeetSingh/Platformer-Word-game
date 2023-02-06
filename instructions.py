be9661246abbd48a983500f5c3e9e1c8def0a6aa
RoopjeetSingh <roopjeetsingh2004@gmail.com>
2023-02-05 20:25:44 -0500

    changes

--- instructions.py
+++ instructions.py
@@ -3,6 +3,8 @@ import ui_tools
 import screen_size as ss
 import json
 from helpful_functions import blit_text
+from decode_file import decode_file
+import other_small_images
 
 pygame.init()
 font = pygame.font.Font(None, int(ss.SCREEN_WIDTH / 39.72))
@@ -67,17 +69,17 @@ def instructions(screen, back_button_func):
         var = json.load(f)
     help_surface = pygame.Surface((ss.SCREEN_WIDTH * 3, ss.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    background = pygame.image.load("hellop/instruction background.jpg").convert()
+    background = pygame.image.load(decode_file(other_small_images.instruction_bg)).convert()
     background = pygame.transform.scale(background, (ss.SCREEN_WIDTH, ss.SCREEN_HEIGHT))
-    next_button = pygame.transform.scale(pygame.image.load("images/Menu_page/i02_next_button.png").convert_alpha(),
+    next_button = pygame.transform.scale(pygame.image.load(decode_file(other_small_images.next_button)).convert_alpha(),
                                          (ss.SCREEN_WIDTH / 14.3, ss.SCREEN_WIDTH / 9.53))
     disabled_next_button = pygame.transform.scale(
-        pygame.image.load("images/Menu_page/i01_next_button.png").convert_alpha(),
+        pygame.image.load(decode_file(other_small_images.disabled_next_button)).convert_alpha(),
         (ss.SCREEN_WIDTH / 14.3, ss.SCREEN_WIDTH / 9.53))
     previous_button = pygame.transform.flip(next_button, True, False)
     disabled_previous_button = pygame.transform.flip(disabled_next_button, True, False)
 
-    back_image = pygame.transform.scale(pygame.image.load("images/back_button.png").convert_alpha(),
+    back_image = pygame.transform.scale(pygame.image.load(decode_file(other_small_images.back_button)).convert_alpha(),
                                         (ss.SCREEN_WIDTH / 14.3, ss.SCREEN_HEIGHT / 8.4))  # 75, 75
     back_button = ui_tools.Button((20, 20, ss.SCREEN_WIDTH / 19.1, ss.SCREEN_HEIGHT / 10.4), (0, 0, 0),
                                   change_screen, image=back_image,
@@ -137,7 +139,7 @@ def instructions(screen, back_button_func):
                   pygame.font.Font(None, 20), 850, color=(130, 1, 29), alignment="left")
 
         # second screen - how to play the running game
-        blit_text(help_surface, "Instructions:", (1950, 100),
+        blit_text(help_surface, "Instructions", (1950, 100),
                   pygame.font.Font(None, 100), 2200, color=(255, 255, 255))
         help_surface.blit(table, (1450, 150))
         blit_text(help_surface, "RUNNING GAME", (1950, 200),
@@ -154,12 +156,12 @@ def instructions(screen, back_button_func):
                   pygame.font.Font(None, 20), 1950, color=(130, 1, 29), alignment="left")
         blit_text(help_surface, "JUMP: W-KEY OR UP ARROW KEY OR SPACE BAR", (1500,510),
                   pygame.font.Font(None, 20), 1950, color=(130, 1, 29), alignment="left")
-        blit_text(help_surface, "AVOID OBSTACLES LIKE SNOWMAN, TREE, SPIKES BY JUMPING OVER THEM. THERE ARE ALSO POWER-UPS LIKE JUMP-BOOST TO HELP YOU FINISH THE LEVEL.", (1500, 550),
+        blit_text(help_surface, "AVOID OBSTACLES LIKE SNOWMAN, TREE, SPIKES BY JUMPING OVER THEM.\nTHERE ARE ALSO POWER-UPS LIKE JUMP-BOOST TO HELP YOU FINISH THE LEVEL.", (1500, 550),
                   pygame.font.Font(None, 20), 2250, color=(130, 1, 29), alignment="left")
         blit_text(help_surface, "ALSO TRY TO GET THE MYSTERY LETTER, WHICH YOU CAN USE IN THE NEXT PART OF THE GAME, WORD-CONNECT. THIS WOULD ALLOW YOU TO ADD ANY LETTER OF YOUR WISH WHENEVER YOU NEED IT.", (1500, 610),
                   pygame.font.Font(None, 20), 2250, color=(130, 1, 29), alignment="left")
         # fourth screen - how to play the word-connect game
-        blit_text(help_surface, "Instructions:", (3250, 100),
+        blit_text(help_surface, "Instructions", (3250, 100),
                   pygame.font.Font(None, 100), 3500, color=(255, 255, 255))
         help_surface.blit(table, (2750, 150))
         blit_text(help_surface, "WORD-CONNECT", (3250, 200),
